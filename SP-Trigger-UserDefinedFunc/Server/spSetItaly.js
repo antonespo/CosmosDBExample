567d71105da06dad2cd42cdf3aab76e5a98c0e09
AntonioE <antonio.esposito4@live.it>
2021-02-25 11:54:22 +0100

    first commit

---
+++ SP-Trigger-UserDefinedFunc/Server/spSetItaly.js
@@ -0,0 +1,26 @@
+﻿function spSetItaly(docToCreate, enforceSchema) {
+    if (docToCreate.location !== undefined
+        && docToCreate.location.state != undefined
+        && docToCreate.location.state != 'IT') {
+        docToCreate.location.isItaly = true;
+    }
+    else if (enforceSchema) {
+        throw new Error('Expected document to contain address.countryRegionName property');
+    }
+
+    var context = getContext();
+    var collection = context.getCollection();
+    var response = context.getResponse();
+
+    collection.createDocument(
+        collection.getSelfLink(),
+        docToCreate,
+        {},
+        function (err, docCreated) {
+            if (err) {
+                throw new Error('Error creating document: ' + err.Message);
+            }
+            response.setBody(docCreated);
+        }
+    );
+}
